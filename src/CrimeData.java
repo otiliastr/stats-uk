b3e0b8a7a9cf30f8eb7a7731f51df494f892a894
git <sebastianclaici@gmail.com>
2012-10-29 23:02:16 +0000

    Implemented setting region and city data. Assumes both region and city have a CrimeData field

--- src/CrimeData.java
+++ src/CrimeData.java
@@ -1,18 +1,12 @@
+import java.util.ArrayList;
+import parser.*;
+
 public class CrimeData implements StatisticData {
     // Stores the tags associated with crime rates
     private static ArrayList<String> tags;
     // Stores one data corresponding to one region
     private ArrayList<String> data;
-
-    public CrimeData() {
-        tags = new ArrayList<String>();
-        data = new ArrayList<String>();
-    }
-
-    public CrimeData(ArrayList<String> tags, ArrayList<String> data) {
-        this.tags = tags;
-        this.data = data;
-    }
+    private static csvReader reader = new csvReader();
 
     public ArrayList<String> getData() {
         return data;
@@ -20,5 +14,41 @@ public class CrimeData implements StatisticData {
 
     public ArrayList<String> getTags() {
         return tags;
+    }
+
+    /**
+     * @param line Line in csv file containing tags of the data
+     */
+    public void setTags(String line) {
+        tags = reader.parse(line);
+    }
+
+    /*
+     * Sets the crime data for a region. Region is always 
+     * first element of the row.
+     * @param line Line in csv file containing a row of data
+     */
+    public void setRegionData(String line) {
+        data = reader.parse(line);
+        if (!data.get(1).equals("Total"))
+            throw new IllegalArgumentException();
+
+        String regionName = data.get(0);
+        // throw out first two elements as all data stored in them is
+        // mirrored in the region and city classes
+        data.remove(0);
+        data.remove(1);
+        Country.getRegion(regionName).setData(this);
+    }
+
+    public void setCityData(String line) {
+        data = reader.parse(line);
+        String regionName = data.get(0);
+        String cityName = data.get(1);
+        // throw out first two elements as all data stored in them is
+        // mirrored in the region and city classes
+        data.remove(0);
+        data.remove(1);
+        Country.getRegion(regionName).getCity(cityName).setData(this);
     }
 }
