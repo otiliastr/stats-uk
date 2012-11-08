cd4239e725b81d8723cf9454f30cf175f77ee42c
git <sebastianclaici@gmail.com>
2012-11-08 15:50:22 +0000

    Made the listener explicit by creating a class specifically for it. Also some more formatting

--- src/GUI/SearchField.java
+++ src/GUI/SearchField.java
@@ -24,52 +24,7 @@ public class SearchField extends JPanel{
 
         tf = (JTextField) combo.getEditor().getEditorComponent();
         tf.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        tf.addKeyListener(new KeyAdapter() {
-            public void keyTyped(KeyEvent e) {
-                EventQueue.invokeLater(new Runnable() {
-                    public void run() {
-                        String text = tf.getText();
-                        if(text.length()==0) {
-                            combo.hidePopup();
-                            setModel(new DefaultComboBoxModel(v), "");
-                        }else{
-                            DefaultComboBoxModel m = getSuggestedModel(v, text);
-                            if(m.getSize()==0 || hide_flag) {
-                                combo.hidePopup();
-                                hide_flag = false;
-                            }else{
-                                setModel(m, text);
-                                combo.showPopup();
-                            }
-                        }
-                    }
-                });
-            }
-
-            public void keyPressed(KeyEvent e) {
-                String text = tf.getText();
-                int code = e.getKeyCode();
-                if(code==KeyEvent.VK_ENTER) {
-                    if(!v.contains(text)) {
-                        v.addElement(text);
-                        Collections.sort(v);
-                        setModel(getSuggestedModel(v, text), text);
-                    }
-                    hide_flag = true; 
-                }else if(code==KeyEvent.VK_ESCAPE) {
-                    hide_flag = true; 
-                }else if(code==KeyEvent.VK_RIGHT) {
-                    for(int i=0;i<v.size();i++) {
-                        String str = v.elementAt(i);
-                        if(str.startsWith(text)) {
-                            combo.setSelectedIndex(-1);
-                            tf.setText(str);
-                            return;
-                        }
-                    }
-                }
-            }
-        });
+        tf.addKeyListener(new DropdownSearchListener());
 
         String[] regions = {"England", "West Midlands", "East Midlands", "North East England",
             "North West England", "South East England", "South West England", "London", 
@@ -112,5 +67,52 @@ public class SearchField extends JPanel{
             if(s.startsWith(text)) m.addElement(s);
         }
         return m;
+    }
+    
+    class DropdownSearchListener extends KeyAdapter {
+        public void keyTyped(KeyEvent e) {
+            EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    String text = tf.getText();
+                    if (text.length() == 0) {
+                        combo.hidePopup();
+                        setModel(new DefaultComboBoxModel(v), "");
+                    } else {
+                        DefaultComboBoxModel m = getSuggestedModel(v, text);
+                        if (m.getSize() == 0 || hide_flag) {
+                            combo.hidePopup();
+                            hide_flag = false;
+                        } else {
+                            setModel(m, text);
+                            combo.showPopup();
+                        }
+                    }
+                }
+            });
+        }
+
+        public void keyPressed(KeyEvent e) {
+            String text = tf.getText();
+            int code = e.getKeyCode();
+            if (code == KeyEvent.VK_ENTER) {
+                if (!v.contains(text)) {
+                    v.addElement(text);
+                    Collections.sort(v);
+                    setModel(getSuggestedModel(v, text), text);
+                }
+                hide_flag = true; 
+            }else if (code==KeyEvent.VK_ESCAPE) {
+                hide_flag = true; 
+            }else if (code == KeyEvent.VK_RIGHT) {
+                for(int i = 0; i < v.size(); i++) {
+                    String str = v.elementAt(i);
+                    if(str.startsWith(text)) {
+                        combo.setSelectedIndex(-1);
+                        tf.setText(str);
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
